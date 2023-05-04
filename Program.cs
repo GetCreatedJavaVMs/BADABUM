a48529d322f49ff91df3402793373c8bfc21b51e
GetCreatedJavaVMs <130655015+GetCreatedJavaVMs@users.noreply.github.com>
2023-05-04 13:15:29 +0300

    bubble sort mega array
    yeeep

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DemidenkoProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 5, 2, 7, 3, 1, 8, 6, 9, 4 };
+        int temp;
+
+        Console.WriteLine("Исходный массив:");
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+
+        // Сортировка методом пузырька
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = 0; j < array.Length - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("\nОтсортированный массив:");
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.ReadLine();
+    }
+}
